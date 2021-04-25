3b30d4561efaa1315871fed0b469a90bc46f66f0
Fyodor Sizov <f.sizov@yandex.ru>
2021-04-25 15:58:54 +0300

    added calibration screen (draft) email password auth (1 step)

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'colors.dart';
 
 Future<void> main() async {
@@ -16,8 +17,8 @@ Future<void> main() async {
     routes: {
       // When navigating to the "/" route, build the StartScreen widget.
       '/': (context) => StartScreen(),
-      // When navigating to the "/second" route, build the CheckAuthScreen widget.
-      '/second': (context) => CheckAuthScreen(),
+      // When navigating to the "/calibration" route, build the CalibrationScreen widget.
+      '/calibration': (context) => CalibrationScreen(),
       '/email-signin': (content) => EmailSignInScreen(),
     },
   ),
@@ -29,7 +30,7 @@ class StartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     new Future.delayed(const Duration(seconds: 3), () {
-      Navigator.pushNamed(context, '/second');
+      Navigator.pushNamed(context, '/calibration');
     });
 
     return Scaffold(
@@ -61,7 +62,7 @@ class StartScreen extends StatelessWidget {
   }
 }
 
-class CheckAuthScreen extends StatelessWidget {
+class CalibrationScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     if (FirebaseAuth.instance.currentUser == null) {
@@ -69,19 +70,82 @@ class CheckAuthScreen extends StatelessWidget {
     }
     return Scaffold(
       appBar: AppBar(
-        title: Text("Second Screen"),
+        title: Text("Size Adviser"),
+        backgroundColor: sa_blue,
       ),
       body: Center(
-        child: ElevatedButton(
-          // Within the CheckAuthScreen widget
-          onPressed: () {
-            // Navigate back to the first screen by popping the current route
-            // off the stack.
-            Navigator.pop(context);
-          },
-          child: Text('Hello, ' + FirebaseAuth.instance.currentUser.toString()),
-        ),
-      ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              child: Text(
+                "Welcome!",
+                style: TextStyle(
+                  color: sa_blue,
+                  fontSize: 30,
+                ),
+              ),
+              margin: EdgeInsets.only(bottom: 25.0),
+            ),
+            Text(
+              "To recommend sizes we need to calibrate your profile.",
+              style: TextStyle(
+                fontSize: 16
+              ),
+              textAlign: TextAlign.center,
+            ),
+            Container(
+              child: Text(
+                "Choose your gender",
+                style: TextStyle(
+                  fontSize: 16
+                )
+              ),
+              margin: EdgeInsets.only(top: 50.0, bottom: 10.0)
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: () {  },
+                  child: Text(
+                    "MALE",
+                    style: TextStyle(
+                      color: Colors.black
+                    )
+                  ),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.white54)
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {  },
+                  child: Text(
+                      "FEMALE",
+                      style: TextStyle(
+                          color: Colors.black,
+                      )
+                  ),
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(Colors.white54),
+                  ),
+                  width
+                ),
+              ],
+            ),
+            Text(
+                "Please try at least 5 pairs of shoes of different popular brands.",
+                textAlign: TextAlign.center,
+            ),
+            ElevatedButton(
+              onPressed: () {  },
+              child: Text("OK, I understand")
+            )
+          ],
+        )
+      )
     );
   }
 }
@@ -93,13 +157,34 @@ class EmailSignInScreen extends StatefulWidget {
 
 class _EmailSignInScreenState extends State<EmailSignInScreen> {
   bool _invalid_ep = false;
+  final emailController = TextEditingController();
+  final pwdController = TextEditingController();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    pwdController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    void throw_warning() {
+    Future<void> send_signin_data() async {
       setState(() {
-        _invalid_ep = true;
+        _invalid_ep = false;
       });
+      try {
+        UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+            email: emailController.text,
+            password: pwdController.text
+        );
+        Navigator.pushReplacementNamed(context, '/calibration');
+      } on FirebaseAuthException catch (e) {
+        // TODO: may use different 'e.code' values
+        setState(() {
+          _invalid_ep = true;
+        });
+      }
     }
 
     return Scaffold(
@@ -113,19 +198,21 @@ class _EmailSignInScreenState extends State<EmailSignInScreen> {
                   border: UnderlineInputBorder(),
                   labelText: 'Email'
                 ),
+                controller: emailController,
               ),
               TextFormField(
                 decoration: InputDecoration(
                     border: UnderlineInputBorder(),
                     labelText: 'Password',
                 ),
+                controller: pwdController,
                 obscureText: true,
                 enableSuggestions: false,
                 autocorrect: false,
               ),
               ElevatedButton(
-                // Within the CheckAuthScreen widget
-                onPressed: throw_warning,
+                // Within the CalibrationScreen widget
+                onPressed: send_signin_data,
                 child: Text("Sign In"),
               ),
               if (_invalid_ep) Text(
